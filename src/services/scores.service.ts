0d995acd3feef2961d806701a875096f79544b02
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2024-09-11 10:54:33 +0200

    score

--- src/services/scores.service.ts
+++ src/services/scores.service.ts
@@ -1,7 +1,16 @@
 import { ImageAnalyseInfos } from "../types/image";
 
 export const calculateScore = (imageInfos: ImageAnalyseInfos) => {
-  // ! Ajouter la logic de scoring
-  console.log(imageInfos);
-  return 100;
+  if(imageInfos.number_of_pixel_with_delta_minus_or_equal_20 > 0.2 * 600){
+    if(imageInfos.number_of_pixel_with_delta_minus_or_equal_5 > 0.1 * 600){
+      console.log("Score : ", 100)
+      return(100)
+    }else{
+      console.log("Score : ", 100 - imageInfos.average_distance)
+      return(100 - imageInfos.average_distance)
+    }
+  }else{
+    console.log("Score : ", 100 - imageInfos.average_distance)
+    return(100 - imageInfos.average_distance)
+  }
 };
