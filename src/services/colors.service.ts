2d5b11211fc0f7effb8d8f5ef8db0ee8f17f77d5
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-09-12 11:15:00 +0200

    refine(score): Refine the score algorythme to handle gap between score

--- src/services/colors.service.ts
+++ src/services/colors.service.ts
@@ -1,71 +1,71 @@
-import { ColorLab } from '../types/color';
+import { ColorLab } from "../types/color";
 
 function seededRandom(seed: number): number {
-	let x = Math.sin(seed) * 10000;
-	return x - Math.floor(x);
+  let x = Math.sin(seed) * 10000;
+  return x - Math.floor(x);
 }
 
 export function generateColorBasedOnDay() {
-	const date = new Date();
-	const year = date.getFullYear();
-	const dayOfYear = Math.floor(
-		(Date.UTC(year, date.getMonth(), date.getDate()) - Date.UTC(year, 0, 0)) /
-			24 /
-			60 /
-			60 /
-			1000
-	);
+  const date = new Date();
+  const year = date.getFullYear();
+  const dayOfYear = Math.floor(
+    (Date.UTC(year, date.getMonth(), date.getDate()) - Date.UTC(year, 0, 0)) /
+      24 /
+      60 /
+      60 /
+      1000
+  );
 
-	const seed = year * 1000 + dayOfYear;
+  const seed = year * 1000 + dayOfYear;
 
-	const luminance = Math.floor(seededRandom(seed) * 101); // Luminance between 0 and 100
-	const greenRed = Math.floor(seededRandom(seed + 1) * 256) - 128; // greenRed between -128 and 127
-	const blueYellow = Math.floor(seededRandom(seed + 2) * 256) - 128; // blueYellow between -128 and 127
+  const luminance = Math.floor(seededRandom(seed) * 101); // Luminance between 0 and 100
+  const greenRed = Math.floor(seededRandom(seed + 1) * 256) - 128; // greenRed between -128 and 127
+  const blueYellow = Math.floor(seededRandom(seed + 2) * 256) - 128; // blueYellow between -128 and 127
 
-	const color: ColorLab = {
-		luminance,
-		greenRed,
-		blueYellow,
-	};
+  const color: ColorLab = {
+    luminance,
+    greenRed,
+    blueYellow,
+  };
 
-	return color;
+  return color;
 }
 
 export const convertLABToRGB = (lab: number[]): number[] => {
-	let [l, gr, by] = lab;
+  let [l, gr, by] = lab;
 
-	// Convert LAB to XYZ
-	let y = (l + 16) / 116;
-	let x = gr / 500 + y;
-	let z = y - by / 200;
+  // Convert LAB to XYZ
+  let y = (l + 16) / 116;
+  let x = gr / 500 + y;
+  let z = y - by / 200;
 
-	const xyz = [x, y, z].map(function (value) {
-		const valueCubed = Math.pow(value, 3);
-		return valueCubed > 0.008856 ? valueCubed : (value - 16 / 116) / 7.787;
-	});
+  const xyz = [x, y, z].map(function (value) {
+    const valueCubed = Math.pow(value, 3);
+    return valueCubed > 0.008856 ? valueCubed : (value - 16 / 116) / 7.787;
+  });
 
-	x = xyz[0] * 95.047;
-	y = xyz[1] * 100.0;
-	z = xyz[2] * 108.883;
+  x = xyz[0] * 95.047;
+  y = xyz[1] * 100.0;
+  z = xyz[2] * 108.883;
 
-	// Convert XYZ to RGB
-	x /= 100;
-	y /= 100;
-	z /= 100;
+  // Convert XYZ to RGB
+  x /= 100;
+  y /= 100;
+  z /= 100;
 
-	let r = x * 3.2406 + y * -1.5372 + z * -0.4986;
-	let g = x * -0.9689 + y * 1.8758 + z * 0.0415;
-	let b = x * 0.0557 + y * -0.204 + z * 1.057;
+  let r = x * 3.2406 + y * -1.5372 + z * -0.4986;
+  let g = x * -0.9689 + y * 1.8758 + z * 0.0415;
+  let b = x * 0.0557 + y * -0.204 + z * 1.057;
 
-	[r, g, b] = [r, g, b].map(function (value) {
-		return value > 0.0031308
-			? 1.055 * Math.pow(value, 1 / 2.4) - 0.055
-			: value * 12.92;
-	});
+  [r, g, b] = [r, g, b].map(function (value) {
+    return value > 0.0031308
+      ? 1.055 * Math.pow(value, 1 / 2.4) - 0.055
+      : value * 12.92;
+  });
 
-	[r, g, b] = [r, g, b].map(function (value) {
-		return Math.min(Math.max(0, value), 1) * 255;
-	});
+  [r, g, b] = [r, g, b].map(function (value) {
+    return Math.min(Math.max(0, value), 1) * 255;
+  });
 
-	return [Math.round(r), Math.round(g), Math.round(b)];
+  return [Math.round(r), Math.round(g), Math.round(b)];
 };
--- src/types/image.ts
+++ src/types/image.ts
@@ -1,6 +1,15 @@
 export interface ImageAnalyseInfos {
-  average_distance: number;
+  average_distance_max_delta_percent: number;
   color: number[];
-  number_of_pixel_with_delta_minus_or_equal_20: number;
-  number_of_pixel_with_delta_minus_or_equal_5: number;
+  number_of_pixel_with_delta_20_10_percent_max_delta: number;
+  number_of_pixel_with_delta_30_20_percent_max_delta: number;
+  number_of_pixel_with_delta_40_30_percent_max_delta: number;
+  number_of_pixel_with_delta_50_40_percent_max_delta: number;
+  number_of_pixel_with_delta_60_50_percent_max_delta: number;
+  number_of_pixel_with_delta_70_60_percent_max_delta: number;
+  number_of_pixel_with_delta_80_70_percent_max_delta: number;
+  number_of_pixel_with_delta_90_80_percent_max_delta: number;
+  number_of_pixel_with_delta_upper_90_percent_max_delta: number;
+  number_of_pixel_with_delta_minus_or_equal_10_percent_max_delta: number;
+  size: number;
 }
