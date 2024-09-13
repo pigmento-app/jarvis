a1d30be44431619c98e40aa10cd5f602ec6ebcdb
ReaSlyn <reaslynn@gmail.com>
2024-09-13 02:25:35 +0200

    feat: notifications using expo

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -1,39 +1,60 @@
-import express, { Request, Response } from "express";
-import admin from "firebase-admin";
-import cron from "node-cron";
+import express, { Request, Response } from 'express';
+import cron from 'node-cron';
+import Expo from 'expo-server-sdk';
 
-const serviceAccount = require("../../pigmento-d89f9-firebase-adminsdk-4c7o5-3f1c994b8c.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const expo = new Expo();
 
 const tokens: string[] = [];
 
 const router = express.Router();
 
-router.post("/register", (req: Request, res: Response) => {
-  tokens.push(req.body.token);
-  res.status(200).json({ message: "Successfully registered FCM Token!" });
+router.post('/register', (req: Request, res: Response) => {
+	tokens.push(req.body.token);
 });
 
-const sendNotification = async () => {
-  try {
-    await admin.messaging().sendEachForMulticast({
-      tokens,
-      notification: {
-        title: "New Notification",
-        body: "This is a new notification!",
-        imageUrl: "https://via.placeholder.com/150",
-      },
-    });
-  } catch (err) {
-    console.error(err);
-  }
+const sendNotificationWithResponse = async (res: Response) => {
+	// Filter out invalid tokens
+	const validTokens = tokens.filter((token) => Expo.isExpoPushToken(token));
+
+	if (validTokens.length === 0) {
+		return res.status(400).send('No valid Expo push tokens provided');
+	}
+
+	try {
+		sendNotification();
+
+		res.status(200).send('Notifications sent');
+	} catch (error) {
+		console.error('Error sending notifications:', error);
+		res.status(500).send('Error sending notifications');
+	}
 };
 
-router.get("/testSend", sendNotification);
+const sendNotification = async () => {
+	const validTokens = tokens.filter((token) => Expo.isExpoPushToken(token));
 
-cron.schedule("0 9-22 * * *", sendNotification);
+	// Create messages for each token
+	const messages = validTokens.map((token) => ({
+		to: token,
+		title: '⚠️ Time to Pigmento ⚠️',
+		body: '5 min left to capture a Pigmento and see how close your were to the real color!',
+	}));
+
+	// Send notifications
+	const chunks = expo.chunkPushNotifications(messages);
+	const tickets = [];
+	for (const chunk of chunks) {
+		const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+		tickets.push(...ticketChunk);
+	}
+
+	console.log('Notifications sent:', tickets);
+};
+
+router.get('/testSend', (req: Request, res: Response) => {
+	sendNotificationWithResponse(res);
+});
+
+cron.schedule('0 9-22 * * *', sendNotification);
 
 export default router;
